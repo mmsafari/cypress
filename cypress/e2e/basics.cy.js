d95eae7b494964c687f775d75af886c743bea10b
mmasafari <mmsafariiranian@gmail.com>
2023-09-22 15:13:18 +0330

    initilize main test project

---
+++ cypress/e2e/basics.cy.js
@@ -0,0 +1,5 @@
+describe('task page', () => {
+  it('should render the main image', () => {
+    cy.visit('http://127.0.0.1:5173/')
+  })
+})
--- cypress/e2e/first-test.cy.js
+++
@@ -1,6 +0,0 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://127.0.0.1:5173/')
-    cy.get('li').should('have.length', 6)
-  })
-})
