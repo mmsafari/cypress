7ffc818e228711e3aae02fad709052ffe89b9b7c
mmasafari <mmsafariiranian@gmail.com>
2023-09-24 21:58:41 +0330

    add test for validate the form input

--- cypress/e2e/contact.cy.js
+++ cypress/e2e/contact.cy.js
@@ -8,12 +8,24 @@ describe("contact form", () => {
     cy.get('[data-cy="contact-input-email"]').type("test@test.com");
     cy.get('[data-cy="contact-btn-submit"]').then((el) => {
       expect(el.attr("disabled")).to.be.undefined;
-      expect(el.text()).to.eq('Send Message');
+      expect(el.text()).to.eq("Send Message");
     });
+
+    //  cy.get('[data-cy="contact-input-email"]').type("test@test.com{enter}");
     //  cy.get('[data-cy="contact-btn-submit"]').should('not.have.attr', 'disabled');
     cy.get('[data-cy="contact-btn-submit"]').as("submitBtn");
     cy.get("@submitBtn").click();
     cy.get("@submitBtn").contains("Sending...");
     cy.get("@submitBtn").should("have.attr", "disabled");
+  });
+  it("should validate the form input", () => {
+    cy.visit("http://127.0.0.1:5173/about");
+    cy.get('[data-cy="contact-btn-submit"]').click();
+    
+    cy.get('[data-cy="contact-btn-submit"]').then((el) => {
+      expect(el).to.not.have.attr('disabled');
+      expect(el.text()).to.not.eq("Sending...");
+    });
+    cy.get('[data-cy="contact-btn-submit"]').contains("Send Message");
   });
 });
